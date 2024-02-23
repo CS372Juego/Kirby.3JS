16f4330bdd39d0b9a38a0c30aeef7fabda4dc089
cobblestone5517 <kjy11gold@gmail.com>
2024-02-22 22:32:15 -0500

    Add world1 structure

--- src/world1.js
+++ src/world1.js
@@ -1,21 +1,98 @@
 import * as THREE from 'three';
 import { Colors } from './color.js';
 
-export const LAND_LENGTH = 200;
+export const LAND_LENGTH = 300;
 export const LAND_OFFSET = 15;
 export const LAND_BEGIN_X = -LAND_LENGTH / 2 + LAND_OFFSET;
 export const LAND_END_X = LAND_LENGTH / 2 - LAND_OFFSET;
 
+// Global variables for reuse
+let geometry, plain;
+let material = new THREE.MeshPhongMaterial({ color: Colors.green });
+const LAND_WIDTH = 10;
+
 export function createWorld1(scene) {
     let world1 = new THREE.Group();
-    let geometry = new THREE.BoxGeometry(LAND_LENGTH, 8, 10);
-    let material = new THREE.MeshPhongMaterial({ color: Colors.green });
-    let plain = new THREE.Mesh(geometry, material);
+
+    geometry = new THREE.BoxGeometry(LAND_LENGTH, 10, LAND_WIDTH);
+    plain = new THREE.Mesh(geometry, material);
     plain.position.set(0, 0, 0);
     world1.add(plain);
 
-    // TO DO: Add more objects to world1
+    geometry = new THREE.BoxGeometry(40, 50, LAND_WIDTH);
+    plain = new THREE.Mesh(geometry, material);
+    plain.position.set(LAND_BEGIN_X-LAND_OFFSET-10, 20, 0);
+    world1.add(plain);
 
+    geometry = new THREE.BoxGeometry(30, 12, LAND_WIDTH);
+    plain = new THREE.Mesh(geometry, material);
+    plain.position.set(LAND_BEGIN_X, 3, 0);
+    world1.add(plain);
+
+    geometry = new THREE.BoxGeometry(20, 5, LAND_WIDTH);
+    plain = new THREE.Mesh(geometry, material);
+    plain.position.set(LAND_BEGIN_X + 23.5, 3, 0);
+    plain.rotation.z = -Math.PI / 8;
+    world1.add(plain);
+
+    geometry = new THREE.BoxGeometry(5, 10, LAND_WIDTH);
+    plain = new THREE.Mesh(geometry, material);
+    plain.position.set(LAND_BEGIN_X + 50, 5, 0);
+    world1.add(plain);
+
+    geometry = new THREE.BoxGeometry(5, 20, LAND_WIDTH);
+    plain = new THREE.Mesh(geometry, material);
+    plain.position.set(LAND_BEGIN_X + 54.9, 5, 0);
+    world1.add(plain);
+
+    geometry = new THREE.BoxGeometry(20, 20, LAND_WIDTH);
+    plain = new THREE.Mesh(geometry, material);
+    plain.position.set(LAND_BEGIN_X + 80, 5, 0);
+    world1.add(plain);
+
+    geometry = new THREE.BoxGeometry(10, 3, LAND_WIDTH);
+    plain = new THREE.Mesh(geometry, material);
+    plain.position.set(LAND_BEGIN_X + 110, 14, 0);
+    world1.add(plain);
+
+    geometry = new THREE.BoxGeometry(10, 3, LAND_WIDTH);
+    plain = new THREE.Mesh(geometry, material);
+    plain.position.set(LAND_BEGIN_X + 130, 14, 0);
+    world1.add(plain);
+
+    geometry = new THREE.BoxGeometry(10, 3, LAND_WIDTH);
+    plain = new THREE.Mesh(geometry, material);
+    plain.position.set(LAND_BEGIN_X + 150, 14, 0);
+    world1.add(plain);
+
+    geometry = new THREE.BoxGeometry(20, 20, LAND_WIDTH);
+    plain = new THREE.Mesh(geometry, material);
+    plain.position.set(LAND_BEGIN_X + 180, 5, 0);
+    world1.add(plain);
+
+    geometry = new THREE.BoxGeometry(20, 10, LAND_WIDTH);
+    plain = new THREE.Mesh(geometry, material);
+    plain.position.set(LAND_BEGIN_X + 197, 6.4, 0);
+    plain.rotation.z = -Math.PI / 8;
+    world1.add(plain);
+
+    geometry = new THREE.BoxGeometry(30, 12, LAND_WIDTH);
+    plain = new THREE.Mesh(geometry, material);
+    plain.position.set(LAND_BEGIN_X + 210, 3, 0);
+    world1.add(plain);
+
+    geometry = new THREE.BoxGeometry(20, 5, LAND_WIDTH);
+    plain = new THREE.Mesh(geometry, material);
+    plain.position.set(LAND_BEGIN_X + 233, 3, 0);
+    plain.rotation.z = -Math.PI / 8;
+    world1.add(plain);
+
+    geometry = new THREE.BoxGeometry(40, 50, LAND_WIDTH);
+    plain = new THREE.Mesh(geometry, material);
+    plain.position.set(LAND_END_X+LAND_OFFSET+10, 20, 0);
+    world1.add(plain);
+
+    // TO DO: Add more objects to world1
 
     scene.add(world1);
 }
