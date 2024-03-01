fc267f517ba2c4e1d75dc1baaa72e1ab3f957ee5
cobblestone5517 <kjy11gold@gmail.com>
2024-02-29 22:13:28 -0500

    Add licenses

--- src/main.js
+++ src/main.js
@@ -4,6 +4,9 @@
  * Jason Chew, Daniel Kim
  * Feb. 2024
  * 3D Models provided by Quaternius: https://poly.pizza/u/Quaternius
+ * 3D Models provided by Nintendo-HAL Laboratory: https://www.models-resource.com/wii/kirbysreturntodreamland/
+ * 3D Model provided by KK's Studio: https://sketchfab.com/3d-models/kirby-animated-3-animations-3bfe05c6a04f43a19cf377e0a4a171d7
+ * Background music from Kirby Super Star Ultra: https://downloads.khinsider.com/game-soundtracks/album/kirby-super-star-ultra
  */
 
 import * as THREE from 'three';
