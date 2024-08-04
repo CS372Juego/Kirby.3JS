ca62c57efd08c7c6b1cd4f2a745061246cd3309f
Cobblestone5517 <kjy11gold@gmail.com>
2024-08-04 23:04:02 +0900

    AMEND: fix file directory

--- src/world/world2.js
+++ src/world/world2.js
@@ -1,5 +1,5 @@
 import * as THREE from 'three';
-import { Tree, Spikes, StarBox, Door, loadTreeModel, loadDoorModel } from '../src/structure.js';
+import { Tree, Spikes, StarBox, Door, loadTreeModel, loadDoorModel } from '../structure.js';
 
 const LAND_WIDTH = 20;
 export const WORLD2_OFFSET_X = 350;
